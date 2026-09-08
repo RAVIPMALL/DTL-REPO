1e03147e9096b9550d3f410560a053ad409afff3
Ravi Pratap Mall <mallrvprt@gmail.com>
2026-09-08 14:05:35 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,27 @@
 plugins {
- id("com.android.application") version "8.7.3" apply false
- id("org.jetbrains.kotlin.android") version "2.0.21" apply false
- id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.dtl.connect"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.dtl.connect"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 8
+        versionName = "0.8.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = "21"
+    }
 }
